aaa2cd6c4761b54836d753a676c35f415509e768
Edwan13Carrillo <edwan13alexander@gmail.com>
2025-08-21 21:54:26 -0500

    avance navbar y contenedores

--- js/controlador.js
+++ js/controlador.js
@@ -1,24 +1,52 @@
 $(document).ready(function() {
     // Obtener el rol del localStorage
     let rol = localStorage.getItem("rol");
-    console.log("Rol detectado:", rol);
+    let username = localStorage.getItem("username");
+    console.log("Rol detectado:", rol, "usuario detectado:", username);
 
     // Verificar si hay una sesión activa
     if (!rol) {
         window.location.href = 'login.html';
         return;
     }
+    
+    // Mostrar el rol y el nombre del usuario en la navbar.
+    $("#nombreDeUsuario").text(username);
+    $("#rolDeUsuario").text(rol);
 
-    // Ocultar todas las secciones inicialmente
-    $('.usuarios, .administradores').hide();
+    //Ocualtar contenedores
+    $('#panelProductos, #AjustesContenedor, #panelNomina').hide();
 
-    // Configurar la visualización según el rol
-    if (rol === 'admin') {
-        $('.administradores').fadeIn(1000);
-    } else if (rol === 'usuario') {
-        $('.usuarios').fadeIn(1000);
-        // Agregar mensaje de bienvenida para usuario
-        $('body').prepend(`
-        `);
-    }
+     // Manejador de eventos para los botones del menú - Ajustes
+    $('#btn-ajustes').click(function() {
+        $('.navItem').removeClass('active');
+        $(this).addClass('active');
+        
+        $('#AjustesContenedor').show();
+        $('#panelProductos').hide();
+        $('#panelNomina').hide();
+
+    });
+    // Manejador de eventos para los botones del menú - Productos
+    $('#btn-productos').click(function() {
+        $('.navItem').removeClass('active');
+        $(this).addClass('active');
+
+        $('#panelProductos').show();
+        $('#AjustesContenedor').hide();
+        $('#panelNomina').hide();
+
+    });
+
+    // Manejador de eventos para los botones del menú - Nómina
+    $('#btn-nomina').click(function() {
+        $('.navItem').removeClass('active');
+        $(this).addClass('active');
+
+        $('#panelNomina').show();
+        $('#panelProductos').hide();
+        $('#AjustesContenedor').hide();
+
+    });
+    
 });
--- js/login.js
+++ js/login.js
@@ -4,11 +4,10 @@ $(document).ready(function() {
     
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-    const errorMessage = $('#errorMessage');
 
     // Lista de usuarios con sus roles
     const usuarios = [
-        { username: 'admin', password: '123456', rol: 'admin' },
+        { username: 'matt', password: '123456', rol: 'admin' },
         { username: 'juan', password: 'user123', rol: 'usuario' },
         { username: 'maria', password: 'user456', rol: 'usuario' },
         { username: 'supervisor', password: 'super789', rol: 'admin' }
@@ -17,37 +16,20 @@ $(document).ready(function() {
     // Buscar el usuario en la lista
     const usuarioEncontrado = usuarios.find(user => 
         user.username === username && user.password === password
-    );
+    );    
 
-    let rolUsuario = usuarioEncontrado.rol;
-    //alert('el usuario si exite con el rol: ' + rolUsuario);
-
-    //Guardar rol en localStorage y redirigir al index.html
+    //Guardar rol y el usuario en localStorage y redirigir al index.html
     if (usuarioEncontrado) {
+        let rolUsuario = usuarioEncontrado.rol;
+        let nombreUsuario = usuarioEncontrado.username;
+
         localStorage.setItem('rol', rolUsuario);
+        localStorage.setItem('username', nombreUsuario);
         window.location.href = 'index.html';
 
     } else {
-        alert('Tas pndjo si crees que vas a entrar asi');
-        return
+        console.log("que cosas");
     }
 
-    
-
-    /*
-    if (usuarioEncontrado) {
-        alert(`¡Bienvenido ${username}! Rol: ${usuarioEncontrado.rol}`);
-        errorMessage.style.display = 'none';
-        
-        // Redirigir según el rol
-        if (usuarioEncontrado.rol === 'admin') {
-            window.location.href = '../Pestañas/admin.html';
-        } else {
-            window.location.href = '../Pestañas/productos.html';
-        }
-    } else {
-        errorMessage.style.display = 'block';
-    }
-    */
     });
 });
